26fd1f070dbd1ff4e1278aaac35e6346bb48090f
Tomáš Kolinger <tomas@kolinger.name>
2013-09-16 01:55:52 +0200

    optional https

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -11,17 +11,36 @@ class RouterFactory
 {
 
 	/**
+	 * @var bool
+	 */
+	private $useHttps;
+
+
+	/**
+	 * @param bool $useHttps
+	 */
+	public function __construct($useHttps)
+	{
+		$this->useHttps = $useHttps;
+	}
+
+	/**
 	 * @return \Nette\Application\IRouter
 	 */
 	public function createRouter()
 	{
 		$router = new \Nette\Application\Routers\RouteList();
 
-		$router[] = new Route('login', 'User:login', Route::SECURED);
-		$router[] = new Route('logout', 'User:logout', Route::SECURED);
-		$router[] = new Route('create', 'Account:create', Route::SECURED);
-		$router[] = new Route('view/<id>', 'Account:view', Route::SECURED);
-		$router[] = new Route('<presenter>[/<action>][/<id>]', 'Account:default', Route::SECURED);
+		$flags = 0;
+		if ($this->useHttps) {
+			$flags = Route::SECURED;
+		}
+		
+		$router[] = new Route('login', 'User:login', $flags);
+		$router[] = new Route('logout', 'User:logout', $flags);
+		$router[] = new Route('create', 'Account:create', $flags);
+		$router[] = new Route('view/<id>', 'Account:view', $flags);
+		$router[] = new Route('<presenter>[/<action>][/<id>]', 'Account:default', $flags);
 
 		return $router;
 	}
